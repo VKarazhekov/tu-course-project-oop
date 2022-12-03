dc48d1c9c84c91a3585fe6786085fb3bfdf0d777
vkarazhekov <vasonic100@gmail.com>
2022-12-03 02:10:21 +0200

    Added encapsulation and constructors in the object classes.

--- optics.cpp
+++ optics.cpp
@@ -7,4 +7,45 @@ class Optics
         float thickness;
         float dioptre;
         string name; 
+    public:
+        Optics(){}
+        Optics(string type, float thickness, float dioptre, string name)
+        {
+            this->type=type;
+            this->thickness=thickness;
+            this->dioptre=dioptre;
+            this->name=name;
+        }
+        void setType(string type)
+        {
+            this->type = type;
+        }
+        string getType()
+        {
+            return type;
+        }
+        void setThickness(float thickness)
+        {
+            this->thickness = thickness;
+        }
+        float getThickness()
+        {
+            return thickness;
+        }
+        void setDioptre(float dioptre)
+        {
+            this->dioptre = dioptre;
+        }
+        float getDioptre()
+        {
+            return dioptre;
+        }
+        void setName(string name)
+        {
+            this->name = name;
+        }
+        string getType()
+        {
+            return name;
+        }
 };
